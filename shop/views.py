4f8e8717bc033884d6df20252342f8bb587755b7
alternateSS <potryasnyidjek@gmail.com>
2022-12-02 14:58:13 +0600

    First Commit

---
+++ shop/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def greetings(request):
+    return HttpResponse('Добро пожаловать в наш магазин!')
