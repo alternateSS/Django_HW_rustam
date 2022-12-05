02dec871c85eaaac704e7623afcead52113b3dff
alternateSS <potryasnyidjek@gmail.com>
2022-12-05 13:45:56 +0600

    First Commit

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shop/greetings', views.greetings, name='greetings'),
+    path('shop/greetings/', views.greetings, name='greetings'),
+    path('shop/', views.list_item, name='list_item'),
+    path('shop/<int:id>/', views.detail_item, name='detail_item'),
+
 ]
